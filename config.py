3efd3e9d8c170f6f45ff13edaf81dc45287341ca
AndryFox <97800930+AndryFox@users.noreply.github.com>
2026-08-25 17:43:02 +0200

    Fix syntax error in ALIASES dictionary

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ MIO_ID = 715247279141027890
 # --- SISTEMA DEGLI ALIAS ---
 ALIASES = {
     "namsarr1": "rasman",
-    "namsar": "rasman,
+    "namsar": "rasman",
     "samu_onchill": "boxato",
     "blaagoosb": "M2xD",
     "m2xd": "M2xD",
